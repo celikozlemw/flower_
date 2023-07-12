d62483e7f720aa80f92f4436dde71eaa9fc080f2
ÖZLEM <celik.ozlem.05062003@gmail.com>
2023-07-12 09:45:22 +0300

    ✽❃✽

---
+++ f.js
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style>
+		body {
+			background-color: black;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 100vh;
+			margin: 0;
+		}
+
+		.flower {
+			position: relative;
+			width: 200px;
+			height: 200px;
+		}
+
+		.petal {
+			position: absolute;
+			width: 40px;
+			height: 120px;
+			background-color: rgb(247, 237, 237);
+			border-radius: 50%;
+			transform-origin: bottom center;
+			box-shadow: 0 0 10px 5px rgba(255, 255, 255, 0.8);
+			animation: lightEffect 2s infinite;
+		}
+
+		.petal:nth-child(1) {
+			transform: rotate(9deg);
+            
+		}
+
+		.petal:nth-child(2) {
+			transform: rotate(27deg);
+		}
+
+		.petal:nth-child(3) {
+			transform: rotate(45deg);
+		}
+
+		.petal:nth-child(4) {
+			transform: rotate(63deg);
+		}
+
+		.petal:nth-child(5) {
+			transform: rotate(81deg);
+		}
+
+		.petal:nth-child(6) {
+			transform: rotate(99deg);
+		}
+
+		.petal:nth-child(7) {
+			transform: rotate(117deg);
+		}
+
+		.petal:nth-child(8) {
+			transform: rotate(135deg);
+		}
+
+		.petal:nth-child(9) {
+			transform: rotate(153deg);
+		}
+
+		.petal:nth-child(10) {
+			transform: rotate(171deg);
+		}
+
+		.petal:nth-child(11) {
+			transform: rotate(189deg);
+		}
+
+		.petal:nth-child(12) {
+			transform: rotate(207deg);
+		}
+
+		.petal:nth-child(13) {
+			transform: rotate(225deg);
+		}
+
+		.petal:nth-child(14) {
+			transform: rotate(243deg);
+		}
+
+		.petal:nth-child(15) {
+			transform: rotate(261deg);
+		}
+
+		.petal:nth-child(16) {
+			transform: rotate(279deg);
+		}
+
+		.petal:nth-child(17) {
+			transform: rotate(297deg);
+		}
+
+		.petal:nth-child(18) {
+			transform: rotate(315deg);
+		}
+
+		.petal:nth-child(19) {
+			transform: rotate(333deg);
+		}
+
+        .petal:nth-child(20) {
+			transform: rotate(351deg) ;
+		}
+        .petal:nth-child(21) {
+			transform: rotate(369deg);
+		}
+        .petal:nth-child(22) {
+			transform: rotate(387deg);
+		}
+        .petal:nth-child(23) {
+			transform: rotate(407deg);
+		}
+        .petal:nth-child(24) {
+			transform: rotate(423deg);
+		}
+        .petal:nth-child(25) {
+			transform: rotate(441deg);
+		}
+        .petal:nth-child(26) {
+			transform: rotate(450deg)scale(1.2);
+            background-color:rgba(11, 138, 0, 0.8);
+            animation: lightEffect-26 2s infinite;
+
+		}
+
+		@keyframes lightEffect {
+			0% {
+				box-shadow: 0 0 10px 5px rgba(255, 255, 255, 0.8);
+			}
+			50% {
+				box-shadow: 0 0 20px 10px rgba(255, 255, 255, 1);
+			}
+			100% {
+				box-shadow: 0 0 10px 5px rgba(255, 255, 255, 0.8);
+			}
+		}
+
+		@keyframes lightEffect-26 {
+			0% {
+				box-shadow: 0 0 10px 5px rgba(11, 138, 0, 0.8);
+			}
+			50% {
+				box-shadow: 0 0 20px 10px rgb(11, 138, 0);
+			}
+			100% {
+				box-shadow: 0 0 10px 5px rgba(11, 138, 0, 0.8);
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="flower">
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+		 <div class="petal"></div>
+		<div class="petal"></div>
+		<div class="petal"></div>
+	</div>
+
+	<script>
+		var petals = document.getElementsByClassName("petal");
+
+function changeColor(element, duration) {
+    var currentColor = 237;
+    var targetColor = 0;
+    var interval = 10;
+    var steps = duration / interval;
+    var stepSize = (currentColor - targetColor) / steps;
+
+    var currentStep = 0;
+    var colorInterval = setInterval(function() {
+        currentColor -= stepSize;
+        element.style.backgroundColor = "rgb(247," + currentColor + "," + currentColor + ")";
+        currentStep++;
+
+        if (currentStep >= steps) {
+            clearInterval(colorInterval);
+        }
+    }, interval);
+}
+
+setTimeout(function() {
+    for (var i = 0; i < petals.length; i++) {
+        if (i !== 25) {
+            changeColor(petals[i], 2000);
+        }
+    }
+}, 20000);
+	</script>
+</body>
+</html>
